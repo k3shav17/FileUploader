534f48e908a13ff44bf5b91fce4288f48a602ed5
k3shav17 <keshavrao0489@gmail.com>
2022-07-01 11:45:34 +0530

    added implementation to call the backend part, for upload and download files

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,10 +1,80 @@
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpEventType,
+} from '@angular/common/http';
 import { Component } from '@angular/core';
+import { saveAs } from 'file-saver';
+import { FileService } from './file.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ui';
+  filenames: string[] = [];
+  fileStatus: { status: string; requestType: string; percent: number };
+  constructor(private fileService: FileService) {}
+
+  // function to upload files
+  onUploadFiles(files: File[]): void {
+    const formData = new FormData();
+
+    for (const file of files) {
+      formData.append('files', file, file.name);
+      this.fileService.upload(formData).subscribe(
+        (event) => {
+          console.log(event);
+          this.reportProgress(event);
+        },
+        (error: HttpErrorResponse) => console.log(error)
+      );
+    }
+  }
+
+  onDownloadFiles(filename: string): void {
+    this.fileService.download(filename).subscribe(
+      (event) => {
+        console.log(event);
+        this.reportProgress(event);
+      },
+      (error: HttpErrorResponse) => console.log(error)
+    );
+  }
+
+  reportProgress(httpEvent: HttpEvent<string[] | Blob>): void {
+    switch (httpEvent.type) {
+      case HttpEventType.UploadProgress:
+        this.updateStatus(httpEvent.loaded, httpEvent.total, 'Uploading');
+        break;
+      case HttpEventType.DownloadProgress:
+        this.updateStatus(httpEvent.loaded, httpEvent.total, 'Downloading');
+        break;
+      case HttpEventType.ResponseHeader:
+        console.log('Header returned', httpEvent);
+        break;
+      case HttpEventType.Response:
+        if (httpEvent.body instanceof Array) {
+          for (const filename of httpEvent.body) {
+            this.filenames.unshift(filename);
+          }
+        } else {
+          // download logic
+          saveAs(
+            new File([httpEvent.body], httpEvent.headers.get('File-Name'), {
+              type: `${httpEvent.headers.get('Content-Type')};charset=utf-8`,
+            })
+          );
+        }
+        break;
+      default:
+        console.log(httpEvent);
+    }
+  }
+  updateStatus(loaded: number, total: number, requestType: string) {
+    this.fileStatus.status = 'progress';
+    this.fileStatus.requestType = requestType;
+    this.fileStatus.percent = Math.round((loaded / total) * 100);
+  }
 }
--- ui/src/app/file.service.ts
+++ ui/src/app/file.service.ts
@@ -10,7 +10,7 @@ export class FileService {
   constructor(private http: HttpClient) {}
 
   // define function to upload files
-  upload(formData: FormData): Observable<HttpEvent<String[]>> {
+  upload(formData: FormData): Observable<HttpEvent<string[]>> {
     return this.http.post<string[]>(`${this.server}/file/upload`, formData, {
       reportProgress: true,
       observe: 'events',
@@ -18,4 +18,11 @@ export class FileService {
   }
 
   // define function to download files
+  download(filename: string): Observable<HttpEvent<Blob>> {
+    return this.http.get(`${this.server}/file/download/${filename}`, {
+      reportProgress: true,
+      observe: 'events',
+      responseType: 'blob',
+    });
+  }
 }
